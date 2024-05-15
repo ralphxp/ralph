657e8d9e50f51a41defbc28fc7062f41279a4964
ralphxp <raphapanchi@gmail.com>
2024-05-15 21:26:20 +0100

    modified:   src/Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -6,17 +6,21 @@ class Engine{
 
     static $cache_path = 'cache/';
     static $cache_enabled = FALSE;
-    static $yields = [];
+    static $sections = '';
+
+    static $tmp = '';
 
     public static function view(
         $viewName, 
         $data = []
     ){
+        
         extract($data, EXTR_SKIP);
         $viewpath = getcwd()."/views/";
         $file = $viewpath.$viewName;
         $cached_file = self::cache($file);
 	    extract($data, EXTR_SKIP);
+        $message = array_key_exists('message', $data)? $data['message']: "";
 	   	require $cached_file;
         
     }
@@ -37,10 +41,15 @@ class Engine{
 
     public static function compileCode($content)
     {
-        $content = str_replace("{{", "<?php ", $content);
+        $content = str_replace("{{--", "<!-- ", $content);
+        $content = str_replace("--}}", " -->", $content);
+        $content = str_replace("{{", "<?= ", $content);
         $content = str_replace("}}", " ?>", $content);
+        $content = str_replace("@php", "<?php ", $content);
+        $content = str_replace("@endphp", " ?>", $content);
         $content = self::parseComment($content);
         $content = self::parseIf($content);
+        $content = self::parseForEach($content);
         $content = self::parseEcho( $content);
 
         return $content;
@@ -55,14 +64,24 @@ class Engine{
 
     public static function parseIf($content)
     {
-        $content = preg_replace('/@if\(\s*(.+?)\s*\)/i', "<?php if($1): ?> ", $content);
-        $content = str_replace('@endif', "<?php endif; ?>", $content);
+        $content = preg_replace('/@if\s*\(\s*(.+?)\s*\)/i', "<?php if($1): ?> ", $content);
+            $content = preg_replace('/@elseif\s*\(\s*(.+?)\s*\)/i', "<?php elseif($1): ?> ", $content);
+                $content = preg_replace('/@else/i', "<?php else: ?> ", $content);
+        $content = preg_replace('/@endif/i', "<?php endif ?> ", $content);
+        return $content;
+    }
+
+    public static function parseForEach($content)
+    {
+        $content = preg_replace('/@foreach\s*\(\s*(.+?)\s*\)/i', "<?php foreach($1): ?> ", $content);
+        $content = preg_replace('/@endforeach/i', "<?php endforeach ?> ", $content);
         return $content;
     }
 
     public static function parseEcho($content)
     {
-        $content = preg_replace('/__\(\s*(.+?)\s*\)/i', ' echo $1; ', $content);
+        $content = preg_replace('/__\(\s*(.+?)\s*\)/i', '$1;', $content);
+      
         return $content;
     }
 
@@ -70,46 +89,70 @@ class Engine{
     {
         $content = self::loadFile($file);
         $code = $content;
+
 		preg_match_all('/[@#](template|extends)\([\s*\'\"](.+?)[\'\"\s*]\)/i', $content, $matches, PREG_SET_ORDER);
         if(count($matches) > 0){
+
             $template = $matches[0][2];
-            $template = getcwd().'/views/'.str_replace(".",'/', $template);
-            $template = file_get_contents($template.'.blade.php');
-            $content = preg_replace('/[@#](template|extends)\(\s*(.+?)\s*\)/i', '', $content);
-        
+
+            
 
             $code = self::parseYield($template, $content);
 		}
+
         
 
         return $code;
     }
 
-    public static function parseYield($code, $content)
+    public static function parseYield($template, $content)
     {
+        $template = getcwd().'/views/'.str_replace(".",'/', $template);
+        $content = preg_replace('/[@#](template|extends)\([\s*\'\"](.+?)[\'\"\s*]\)/i', '', $content);
 
-        preg_match_all('/[@#]yield\([\s*\'\"](.+?)[\s*\'"]\)/i', $code, $matches, PREG_SET_ORDER);
-        
-        foreach ($matches as $yield) {
-            $value = $yield[1];
-            $section = self::parseSection($content, $value);
-            $code = preg_replace('/[@#]yield\([\s*\'\"]'.$value.'[\s*\'"]\)/i', $section, $code);
-        }   
-        
-        return $code;
+        preg_match_all('/[@#]section\([\s*\'\"](.+?)[\s*\'"]\)(.+?)[@#]endsection/s', $content, $sections, PREG_SET_ORDER);
+        $template = file_get_contents($template.'.blade.php');
+        $code = '';
+        foreach($sections as $section => $cod){
+
+            $c = $cod[0];
+            $name = $cod[1];
+            
+            $template = preg_replace('/@yield\([\s*\'\"]'.$name.'[\s*\'\"]\)/i', $c, $template,);
+            
+            
+
+        }
+
+        $template = preg_replace('/@yield\([\s*\'\"](.+?)[\s*\'\"]\)/i', '', $template,);
+
+        return self::parseSection($template);
 
     }
 
-    public static function parseSection($code, $key)
+    public static function parseSection($code)
     {
-        preg_match_all('/[@#]section\([\s*\'\"]'.$key.'[\s*\'"]\)(.+?)[@#]endsection/s', $code, $matches, PREG_SET_ORDER);
-    return ($matches[0][1]);
+        
+        
+        
+        $code = preg_replace('/[@#]section\([\s*\'\"](.+?)[\s*\'"]\)/s', '', $code);
+        $code = preg_replace('/[@#]endsection/s', '', $code);
+        return $code;
+        
     }
 
     public static function loadFile($file){
-        $file = str_replace('.', '/', $file);
-        $content = file_get_contents($file.'.blade.php');
-        return $content;
+        $file = str_replace('.', '/', $file).'.blade.php';
+        if(file_exists($file)) {
+            
+            
+            $content = file_get_contents($file);
+            return $content;
+        } else {
+            echo "View not found";
+            die();
+        }
+        
     }
 
     public static function parseIncludes($content){
@@ -117,7 +160,7 @@ class Engine{
 		preg_match_all('/@(include|includes|require)\([\s*\'\"](.+?)[\'\"\s*]\)/i', $content, $matches, PREG_SET_ORDER);
         
 		foreach ($matches as $value) {
-            $inc = __DIR__.'/views/'.str_replace(".",'/',$value[2]);
+            $inc = getcwd().'/views/'.str_replace(".",'/',$value[2]);
 			$content = str_replace($value[0], self::loadFile($inc), $content);
 		}
 		$content = preg_replace('/@(include|includes|require)\([\s*\'\"](.+?)[\'\"\s*]\)/i', '', $content);
