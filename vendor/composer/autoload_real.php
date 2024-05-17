1862254e243193035960735e48895dbe8b5d8c7d
ralphxp <raphapanchi@gmail.com>
2024-05-17 08:48:29 +0100

    modified:   composer.json new file:   composer.lock modified:   vendor/composer/autoload_real.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfabdb31bfdcf9ea7e7dbeae1f74ac822
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfabdb31bfdcf9ea7e7dbeae1f74ac822', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfabdb31bfdcf9ea7e7dbeae1f74ac822', 'loadClassLoader'));
