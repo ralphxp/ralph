e03019bc20fe041bdf1e720b8f5f61e37ae5b49a
ralphxp <raphapanchi@gmail.com>
2024-06-24 08:44:21 +0100

    modified:   .gitignore new file:   vendor/composer/InstalledVersions.php new file:   vendor/composer/installed.json new file:   vendor/composer/installed.php new file:   vendor/composer/platform_check.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codx/ralph',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '657e8d9e50f51a41defbc28fc7062f41279a4964',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'codx/ralph' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '657e8d9e50f51a41defbc28fc7062f41279a4964',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
